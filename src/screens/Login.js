0d866f092e1f05d47308cf777fdfc2cb813d4330
mauv2sky <mauv2sky@gmail.com>
2021-06-29 19:54:30 +0900

    update login page css

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,11 +1,78 @@
-import { isLoggedInVar } from "../apollo";
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: flex;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`
+
+const WhiteBox = styled.div`
+    background-color: white;
+    border: 1px solid rgb(219, 219, 219);
+    width: 100%;
+`
+
+const TopBox = styled(WhiteBox)`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 35px 40px 25px 40px;
+    margin-bottom: 10px;
+    form {
+        margin-top: 30px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        width: 100%;
+        input {
+            width: 100%;
+            padding: 7px;
+            background-color: whitesmoke;
+            border-radius: 3px;
+            border: 0.5px solid rgb(219, 219, 219);
+            margin-top: 5px;
+            box-sizing: border-box;
+        }
+    }
+`
+
+const BottomBox = styled(WhiteBox)`
+    padding: 20px 0px;
+    text-align: center;
+`
 
 const Login = () => {
     return (
-        <div>
-            <h1>Login</h1>
-            <button onClick={() => isLoggedInVar(true)}>log in</button>
-        </div>
+        <Container>
+            <TopBox>
+                <h1>Instagram</h1>
+                <form>
+                    <input type='text' placeholder="Username" />
+                    <input type='password' placeholder="Password" />
+                    <input type='submit' value="Log in" />
+                </form>
+                <div>
+                    OR
+                </div>
+                <div>
+                    <span>Log in with Facebook</span>
+                </div>
+                <span>Forget password?</span>
+            </TopBox>
+            <BottomBox>
+                <span>Don't have an account?</span>
+                <span>Sign up</span>
+            </BottomBox>
+            <div>
+                <span>Get the app.</span>
+                <button>App Store</button>
+                <button>Google Play</button>
+            </div>
+        </Container>
     )
 }
 export default Login;
