612573bb9b0071b937fe1f129068563e8572f439
mauv2sky <mauv2sky@gmail.com>
2021-06-29 20:41:36 +0900

    update login page css

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -8,6 +8,11 @@ const Container = styled.div`
     flex-direction: column;
 `
 
+const Wrapper = styled.div`
+    max-width: 350px;
+    width: 100%;
+`
+
 const WhiteBox = styled.div`
     background-color: white;
     border: 1px solid rgb(219, 219, 219);
@@ -30,12 +35,24 @@ const TopBox = styled(WhiteBox)`
         width: 100%;
         input {
             width: 100%;
-            padding: 7px;
-            background-color: whitesmoke;
+            padding: 10px;
+            background-color: #fafafa;
             border-radius: 3px;
             border: 0.5px solid rgb(219, 219, 219);
             margin-top: 5px;
             box-sizing: border-box;
+            &::placeholder {
+                font-size: 12px;
+            }
+            &:last-child {
+                border: none;
+                margin-top: 12px;
+                background-color: #0095f6;
+                color: white;
+                text-align: center;
+                padding: 8px 0px;
+                font-weight: 600;
+            }
         }
     }
 `
@@ -45,28 +62,34 @@ const BottomBox = styled(WhiteBox)`
     text-align: center;
 `
 
+const Separator = styled.div`
+
+`
+
 const Login = () => {
     return (
         <Container>
-            <TopBox>
-                <h1>Instagram</h1>
-                <form>
-                    <input type='text' placeholder="Username" />
-                    <input type='password' placeholder="Password" />
-                    <input type='submit' value="Log in" />
-                </form>
-                <div>
-                    OR
-                </div>
-                <div>
-                    <span>Log in with Facebook</span>
-                </div>
-                <span>Forget password?</span>
-            </TopBox>
-            <BottomBox>
-                <span>Don't have an account?</span>
-                <span>Sign up</span>
-            </BottomBox>
+            <Wrapper>
+                <TopBox>
+                    <h1>Instagram</h1>
+                    <form>
+                        <input type='text' placeholder="Username" />
+                        <input type='password' placeholder="Password" />
+                        <input type='submit' value="Log in" />
+                    </form>
+                    <div>
+                        OR
+                    </div>
+                    <div>
+                        <span>Log in with Facebook</span>
+                    </div>
+                    <span>Forget password?</span>
+                </TopBox>
+                <BottomBox>
+                    <span>Don't have an account?</span>
+                    <span>Sign up</span>
+                </BottomBox>
+            </Wrapper>
             <div>
                 <span>Get the app.</span>
                 <button>App Store</button>
