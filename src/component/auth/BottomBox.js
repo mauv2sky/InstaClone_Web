8579e747e27f99f2d063d32c6a46ca803c17465c
mauv2sky <mauv2sky@gmail.com>
2021-07-16 22:38:06 +0900

    shared components for auth

---
+++ src/component/auth/BottomBox.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import { BaseBox } from "../shared";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const SBottomBox = styled(BaseBox)`
+    padding: 30px 20px;
+    text-align: center;
+    a {
+        font-weight: 600;
+        color: ${(props) => props.theme.accent};
+        margin-left: 5px;
+    }
+`
+
+function BottomBox({ cta, link, linkText }) {
+    return (
+        <SBottomBox>
+            <span>{cta}</span>
+            <Link to={link}>{linkText}</Link>
+        </SBottomBox>
+    );
+}
+
+BottomBox.propTypes = {
+    cta: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired,
+    linkText: PropTypes.string.isRequired,
+}
+
+export default BottomBox;
