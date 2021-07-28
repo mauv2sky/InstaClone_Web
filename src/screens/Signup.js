ef31527b4ae12c6dd11461a961a7cde71f5583dc
mauv2sky <mauv2sky@gmail.com>
2021-07-28 19:04:40 +0900

    add mutation, validations

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -1,6 +1,9 @@
+import { gql, useMutation } from "@apollo/client";
+import { useForm } from "react-hook-form";
+import { useHistory } from "react-router-dom";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faInstagram, faFacebookSquare } from "@fortawesome/free-brands-svg-icons"
+import { faInstagram } from "@fortawesome/free-brands-svg-icons"
 import AuthLayout from "../component/auth/AuthLayout";
 import { FatLink } from "../component/shared";
 import Input from "../component/auth/Input";
@@ -10,15 +13,7 @@ import FormBox from "../component/auth/FormBox";
 import BottomBox from "../component/auth/BottomBox";
 import routes from "../routes";
 import PageTitle from "../component/PageTitle";
-
-const FacebookSignup = styled.div`
-    color: #385285;
-    padding-top: 30px;
-    span {
-        margin-left: 10px;
-        font-weight: 600;
-    }
-`
+import FormError from "../component/auth/FormError";
 
 const HeaderContainer = styled.div`
   display: flex;
@@ -29,10 +24,54 @@ const HeaderContainer = styled.div`
 const Subtitle = styled(FatLink)`
   font-size: 16px;
   text-align: center;
-  margin-top: 10px;
+  margin: 10px 0px 10px 0px;
+`;
+
+const CREATE_ACCOUNT_MUTATION = gql`
+    mutation createAccount(
+        $firstName: String!
+        $lastName: String
+        $userName: String!
+        $email: String!
+        $password: String!
+    ) {
+        createAccount(
+            firstName: $firstName
+            lastName: $lastName 
+            userName: $userName
+            email: $email
+            password: $password
+        ) {
+            ok
+            error
+        }
+    }
 `;
 
 const Signup = () => {
+    const history = useHistory();
+    const onCompleted = (data) => {
+        const { userName, password } = getValues();
+        const { createAccount: { ok } } = data
+        if (!ok) {
+            return;
+        }
+        history.push(routes.home, {
+            message: "Account created. Please log in.",
+            userName,
+            password
+        });
+    }
+    const [createAccount, { loading }] = useMutation(CREATE_ACCOUNT_MUTATION, { onCompleted });
+    const { register, handleSubmit, formState, getValues } = useForm({ mode: "onChange" });
+    const onSubmitValid = (data) => {
+        if (loading) {
+            return;
+        }
+        createAccount({
+            variables: { ...data }
+        });
+    };
     return (
         <AuthLayout>
             <PageTitle title="Sign up" />
@@ -41,18 +80,70 @@ const Signup = () => {
                     <FontAwesomeIcon icon={faInstagram} size="3x" />
                     <Subtitle>Sign up to see photos and videos from your friends.</Subtitle>
                 </HeaderContainer>
-                <FacebookSignup>
-                    <FontAwesomeIcon icon={faFacebookSquare} />
-                    <span>Sign up with Facebook</span>
-                </FacebookSignup>
+                <Button
+                    type="button"
+                    onclick="window.location.href='https://www.facebook.com/login';"
+                    value="Log in with Facebook"
+                />
+
                 <Separator />
-                <form>
-                    <Input type='text' name="firstName" placeholder="Firstname" />
-                    <Input type='text' name="lastName" placeholder="Lastname" />
-                    <Input type='text' name="email" placeholder="Email" />
-                    <Input type='text' name="userName" placeholder="Username" />
-                    <Input type='password' name="password" placeholder="Password" />
-                    <Button type='submit' value="Sign up" />
+                <form onSubmit={handleSubmit(onSubmitValid)}>
+                    <Input
+                        {...register("firstName", { required: "First Name is required." })}
+                        type='text'
+                        placeholder="Firstname"
+                        hasError={Boolean(formState.errors?.firstName?.message)}
+                    />
+                    <FormError message={formState.errors?.firstName?.message} />
+                    <Input type='text' name="lastName" placeholder="Lastname (Option)" />
+                    <Input
+                        {...register("email", {
+                            required: "Email is required.",
+                            pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                                message: "Invalid email format."
+                            }
+                        })}
+                        type='email'
+                        placeholder="Email"
+                        hasError={Boolean(formState.errors?.email?.message)}
+                    />
+                    <FormError message={formState.errors?.email?.message} />
+                    <Input
+                        {...register("userName", {
+                            required: "User Name is required.",
+                            minLength: {
+                                value: 3,
+                                message: "Username should be longer than 3 chars."
+                            },
+                            maxLength: {
+                                value: 24,
+                                message: "Username should be shorter than 24 chars."
+                            }
+                        })}
+                        type='text'
+                        placeholder="Username"
+                        hasError={Boolean(formState.errors?.userName?.message)}
+                    />
+                    <FormError message={formState.errors?.userName?.message} />
+                    <Input
+                        {...register("password", {
+                            required: "Password is required.",
+                            pattern: {
+                                value: /^[a-z0-9]{8,20}$/i,
+                                message: "숫자와 영소문자 조합으로 8~20자리를 사용해야 합니다."
+                            }
+                        })}
+                        type='password'
+                        placeholder="Password"
+                        hasError={Boolean(formState.errors?.password?.message)}
+                    />
+                    <FormError message={formState.errors?.password?.message} />
+                    <Button
+                        type='submit'
+                        value={loading ? "Loading.." : "Sign up"}
+                        disabled={!formState.isValid || loading}
+                    />
                 </form>
             </FormBox>
             <BottomBox
