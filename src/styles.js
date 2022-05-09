5ef0b8b0b160916597059f2c6d737573bd336b85
mauv2sky <mauv2sky@gmail.com>
2022-05-09 21:15:45 +0900

    [Modify] photo header css 수정

--- src/styles.js
+++ src/styles.js
@@ -1,19 +1,19 @@
-import { createGlobalStyle } from "styled-components";
-import reset from "styled-reset";
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 
 export const lightTheme = {
-    accent: "#0095f6",
-    bgColor: "#fafafa",
-    boxColor: "white",
-    fontColor: "rgb(38, 38, 38)",
-    borderColor: "rgb(219, 219, 219)"
+    accent: '#0095f6',
+    bgColor: '#fafafa',
+    boxColor: 'white',
+    fontColor: 'rgb(38, 38, 38)',
+    borderColor: 'rgb(219, 219, 219)',
 };
 
 export const darkTheme = {
-    fontColor: "white",
-    bgColor: "#000",
-    accent: "#0095f6",
-    borderColor: "white"
+    fontColor: 'white',
+    bgColor: '#000',
+    accent: '#0095f6',
+    borderColor: 'white',
 };
 
 export const GlobalStyles = createGlobalStyle`
@@ -33,4 +33,4 @@ export const GlobalStyles = createGlobalStyle`
     a {
         text-decoration: none;
     }
-`
+`;
