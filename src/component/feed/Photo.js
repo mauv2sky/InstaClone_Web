c17e52cdef5269cef673f94a560af72eefaffd4c
mauv2sky <mauv2sky@gmail.com>
2022-05-09 21:57:47 +0900

    [Modify] Home의 Photo 부분을 컴포넌트로 분리

---
+++ src/component/feed/Photo.js
@@ -0,0 +1,108 @@
+import { gql, useMutation } from '@apollo/client';
+import { faBookmark, faComment, faHeart, faPaperPlane } from '@fortawesome/free-regular-svg-icons';
+import { faHeart as SolidHeart } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styled from 'styled-components';
+import Avatar from '../Avatar';
+import { FatText } from '../shared';
+import propTypes from 'prop-types';
+
+const PhotoContainer = styled.div`
+    background-color: white;
+    border: solid 1px ${(props) => props.theme.borderColor};
+    border-radius: 4px;
+    margin-bottom: 60px;
+    max-width: 750px;
+`;
+
+const PhotoHeader = styled.div`
+    padding: 15px;
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid rgb(239, 239, 239);
+`;
+
+const UserName = styled(FatText)`
+    margin-left: 15px;
+`;
+
+const PhotoFile = styled.img`
+    min-width: 100%;
+    max-width: 100%;
+`;
+
+const PhotoData = styled.div`
+    padding: 12px 15px;
+`;
+
+const PhotoActions = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    div {
+        display: flex;
+        align-items: center;
+    }
+
+    svg {
+        font-size: 20px;
+    }
+`;
+
+const PhotoAction = styled.div`
+    margin-right: 10px;
+    cursor: pointer;
+`;
+
+const Likes = styled(FatText)`
+    margin-top: 15px;
+    display: block;
+`;
+
+function Photo({ id, user, file, isLiked, likes }) {
+    return (
+        <PhotoContainer key={id}>
+            <PhotoHeader>
+                <Avatar lg url={user.avatar} />
+                <UserName>{user.userName}</UserName>
+            </PhotoHeader>
+            <PhotoFile src={file} />
+            <PhotoData>
+                <PhotoActions>
+                    <div>
+                        <PhotoAction>
+                            <FontAwesomeIcon
+                                style={{ color: isLiked ? 'tomato' : 'inherit' }}
+                                icon={isLiked ? SolidHeart : faHeart}
+                            />
+                        </PhotoAction>
+                        <PhotoAction>
+                            <FontAwesomeIcon icon={faComment} />
+                        </PhotoAction>
+                        <PhotoAction>
+                            <FontAwesomeIcon icon={faPaperPlane} />
+                        </PhotoAction>
+                    </div>
+                    <div>
+                        <FontAwesomeIcon icon={faBookmark} />
+                    </div>
+                </PhotoActions>
+                <Likes>{likes === 1 ? '1 likes' : `${likes} likes`}</Likes>
+            </PhotoData>
+        </PhotoContainer>
+    );
+}
+
+Photo.propTypes = {
+    id: propTypes.number.isRequired,
+    user: propTypes.shape({
+        avatar: propTypes.string,
+        userName: propTypes.string.isRequired,
+    }),
+    file: propTypes.string.isRequired,
+    isLiked: propTypes.bool.isRequired,
+    likes: propTypes.number.isRequired,
+};
+
+export default Photo;
