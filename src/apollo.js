47a1c6118f53bb41d7be8e7d4395de621bea4c6f
mauv2sky <mauv2sky@gmail.com>
2022-05-09 13:12:49 +0900

    [Add] context에 LS의 토큰을 넣어 요청하는 코드 추가

--- src/apollo.js
+++ src/apollo.js
@@ -1,7 +1,8 @@
-import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
+import { ApolloClient, createHttpLink, InMemoryCache, makeVar } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
-const TOKEN = "TOKEN";
-const DARK_MODE = "DARK_MODE";
+const TOKEN = 'TOKEN';
+const DARK_MODE = 'DARK_MODE';
 
 export const isLoggedInVar = makeVar(Boolean(localStorage.getItem(TOKEN)));
 
@@ -17,15 +18,28 @@ export const logUserOut = () => {
 
 export const darkModeVar = makeVar(false);
 export const enableDarkMode = () => {
-    localStorage.setItem(DARK_MODE, "enabled");
+    localStorage.setItem(DARK_MODE, 'enabled');
     darkModeVar(true);
 };
 export const disableDarkMode = () => {
     localStorage.removeItem(DARK_MODE);
     darkModeVar(false);
-}
+};
+
+const httpLink = createHttpLink({
+    uri: 'http://localhost:4000/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+    return {
+        headers: {
+            ...headers,
+            token: localStorage.getItem(TOKEN),
+        },
+    };
+});
 
 export const client = new ApolloClient({
-    uri: "http://localhost:4000/graphql",
-    cache: new InMemoryCache()
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
 });
