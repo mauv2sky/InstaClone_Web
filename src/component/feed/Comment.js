2de9a8f8ec491d71dc14f048e34e659aa2af4147
mauv2sky <mauv2sky@gmail.com>
2022-05-10 18:51:03 +0900

    [Add] 사진 캡션 및 댓글의 해시태그 추출 기능 추가

--- src/component/feed/Comment.js
+++ src/component/feed/Comment.js
@@ -1,18 +1,34 @@
 import styled from 'styled-components';
 import { FatText } from '../shared';
 import PropTypes from 'prop-types';
+import sanitizeHtml from 'sanitize-html';
 
 const CommentContainer = styled.div``;
 
 const PhotoCaption = styled.span`
     margin-left: 10px;
+    mark {
+        background-color: inherit;
+        color: ${(props) => props.theme.accent};
+        cursor: pointer;
+        &:hover {
+            text-decoration: underline;
+        }
+    }
 `;
 
 function Comment({ author, payload }) {
+    const cleanedPayload = sanitizeHtml(payload.replace(/#[\w]+/g, '<mark>$&</mark>'), {
+        allowedTags: ['mark'],
+    });
     return (
         <CommentContainer>
             <FatText>{author}</FatText>
-            <PhotoCaption>{payload}</PhotoCaption>
+            <PhotoCaption
+                dangerouslySetInnerHTML={{
+                    __html: cleanedPayload,
+                }}
+            />
         </CommentContainer>
     );
 }
